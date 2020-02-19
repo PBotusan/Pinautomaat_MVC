d255b4972121da3edc6f1b4077727316c7c0f20a
Paul B <p_botusan@live.nl>
2020-02-19 12:39:34 +0100

    array met picker
    gevuld

--- WebATM/StringArrays/OmschrijvingArray.cs
+++ WebATM/StringArrays/OmschrijvingArray.cs
@@ -18,8 +18,8 @@ namespace WebATM.StringArrays
         public string GekozenOmschrijving { get; set; }
 
 
-        string[] omschrijvingArray = new string[] { "Deen", "Dekamarkt", "Slager", "Vomar", "Primark", "Nintendo store", "4531554352", "4677654336", "9248785923", "Geleteken", "VUE" };
-        string[] plaatsArray = new string[] { "Wervershoof", "Hoorn", "Andijk", "Midwoud", "Amsterdam", "Rotterdam", "Morgenwoud", "Duitsland", "UK" };
+        string[] omschrijvingArray = new string[] { "Deen", "Dekamarkt", "Slager", "Vomar", "Primark", "Nintendo store", "4531554352", "4677654336", "9248785923", "Geleteken", "VUE", "12345654", "54376543", "098765432", "H&M", "Thee winkel", "Spar", "Dumpstore" };
+        string[] plaatsArray = new string[] { "Wervershoof", "Hoorn", "Andijk", "Midwoud", "Amsterdam", "Rotterdam", "Morgenwoud", "Duitsland", "UK", "Urk", "Europasingel", "Alkmaar", "Hurnberg", "Zwaag", "Zwaagdijk", "Venhuizen", "Dorpshallen"  };
 
 
         /// <summary>
